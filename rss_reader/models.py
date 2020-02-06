baca1f3ee663342281e4e99628b51348e000001e
mdarsie <github@darsie.me>
2020-02-06 09:42:45 -0500

    create group model

--- rss_reader/models.py
+++ rss_reader/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Groups(models.Model):
+    collection = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
