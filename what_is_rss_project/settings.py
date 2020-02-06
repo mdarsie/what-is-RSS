745eb4e5cb8e147967bf137e89d24427080e4833
mdarsie <github@darsie.me>
2020-02-06 03:45:36 -0500

    create postgresql db

--- what_is_rss_project/settings.py
+++ what_is_rss_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rss_reader',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'what_is_rss_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rssReader',
+        'USER': 'rssadmin',
+        'PASSWORD': 'i*7LFE>DQ69Q48f',
+        'HOST': 'localhost'
     }
 }
 
