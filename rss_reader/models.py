b8cec59b7351cd698a5c79448bb78f4cc068c89e
mdarsie <github@darsie.me>
2020-02-06 09:54:28 -0500

    create article model

--- rss_reader/models.py
+++ rss_reader/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import CharField
 
 
 class Groups(models.Model):
@@ -11,8 +12,22 @@ class Groups(models.Model):
 
 
 class Feed(models.Model):
-    title = models.CharField(max_length=200)
+    feed_title = models.CharField(max_length=200)
     feed_description = models.TextField(default='')
     feed_link = models.CharField(max_length=200)
     feed_image = models.CharField(max_length=200)
-    published_last = models.CharField(max_length=20)
+    feed_last_published = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    article_title = models.CharField(max_length=200)
+    article_description = models.TextField(default='')
+    article_link = models.CharField(max_length=200)
+    article_image = models.CharField(max_length=200)
+    article_last_published = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
