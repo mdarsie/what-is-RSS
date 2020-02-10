a3006983315c6d1f6e33a71d9d5a2ebf2f0f05a9
mdarsie <github@darsie.me>
2020-02-10 04:44:47 -0500

    restart

--- rss_reader/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Group, Feed, Article
-
-admin.site.register([Group, Feed, Article])
--- rss_reader/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RssReaderConfig(AppConfig):
-    name = 'rss_reader'
