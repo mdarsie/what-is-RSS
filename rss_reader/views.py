adf48c8cf92112d580fc23f2b7f2d2e50830228e
mdarsie <github@darsie.me>
2020-02-10 06:11:39 -0500

    creatre views.py

--- rss_reader/views.py
+++ rss_reader/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Group, Feed, Article
+from .serializers import GroupSerializer, FeedSerializer, ArticleSerializer
+
+class GroupView(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class FeedView(viewsets.ModelViewSet):
+    queryset = Feed.objects.all()
+    serializer_class = FeedSerializer
+
+class ArticleView(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
